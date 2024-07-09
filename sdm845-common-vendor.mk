eca7f78705f0c9527d9b421824a7559f40c8a716
Bruno Martins <bgcngm@gmail.com>
2024-07-09 22:00:20 +0700

    sdm845-common: Add EGL libs symlinks
    Change-Id: I6486b919002556b8b3deb7697d5e1ffcf0d30647

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -719,5 +719,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.gnss@4.0-service
 
 PRODUCT_PACKAGES += \
-    libimscamera_jni_libimscamera_jni_symlink32 \
-    libimsmedia_jni_libimsmedia_jni_symlink32
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
